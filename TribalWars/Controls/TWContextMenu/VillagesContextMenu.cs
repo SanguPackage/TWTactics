d96666168b3d9b71fcc3afa35c509d9afd864232
Laoujin <woutervs@hotmail.com>
2014-07-03 15:08:38 +0200

    VillageSContextMenu: implementation

--- TribalWars/Controls/TWContextMenu/VillagesContextMenu.cs
+++ TribalWars/Controls/TWContextMenu/VillagesContextMenu.cs
@@ -33,11 +33,8 @@ namespace TribalWars.Controls.TWContextMenu
 
         #region Constructors
         public VillagesContextMenu()
-            : base()
         {
-            Items.Add("Pinpoint", null, OnPinpoint);
-            Items.Add("Pinpoint && Center", null, OnCenter);
-            Items.Add("BBCode", null, OnBBCode);
+            Items.Add("BBCode", null, OnBbCode);
         }
         #endregion
 
@@ -55,38 +52,17 @@ namespace TribalWars.Controls.TWContextMenu
 
         #region EventHandlers
         /// <summary>
-        /// Puts the pinpoint on the target villages
-        /// </summary>
-        private void OnPinpoint(object sender, EventArgs e)
-        {
-            if (Villages != null)
-            {
-                World.Default.Map.EventPublisher.SelectVillages(null, Villages, VillageTools.PinPoint);
-            }
-        }
-
-        /// <summary>
-        /// Pinpoints and centers the target villages
-        /// </summary>
-        private void OnCenter(object sender, EventArgs e)
-        {
-            if (Villages != null)
-            {
-                World.Default.Map.EventPublisher.SelectVillages(null, Villages, VillageTools.PinPoint);
-                World.Default.Map.SetCenter(Data.Maps.Display.GetSpan(Villages));
-            }
-        }
-
-        /// <summary>
         /// Create BB code for the villages and put on clipboard
         /// </summary>
-        private void OnBBCode(object sender, EventArgs e)
+        private void OnBbCode(object sender, EventArgs e)
         {
             if (Villages != null)
             {
-                StringBuilder str = new StringBuilder();
+                var str = new StringBuilder();
                 foreach (Village village in Villages)
+                {
                     str.AppendFormat("{0}{1}", village.BBCode(), Environment.NewLine);
+                }
 
                 try
                 {
