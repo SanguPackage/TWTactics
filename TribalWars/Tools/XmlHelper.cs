4b4ea2a86f82acc83542bd71096b09e3d9afc287
Laoujin <woutervs@hotmail.com>
2014-07-04 15:30:19 +0200

    Removed obsolete XmlHelper Serialize/Deserialize extension methods

--- TribalWars/Tools/XmlHelper.cs
+++ TribalWars/Tools/XmlHelper.cs
@@ -67,52 +67,5 @@ namespace TribalWars.Tools
             if (color.IsKnownColor) return color.Name;
             return string.Format("{0};{1};{2}", color.R.ToString(CultureInfo.InvariantCulture), color.G.ToString(CultureInfo.InvariantCulture), color.B.ToString(CultureInfo.InvariantCulture));
         }
-
-        public static string Serialize<T>(this T value) where T : class
-        {
-            if (value == null)
-            {
-                return "";
-            }
-
-            try
-            {
-                var xmlserializer = new XmlSerializer(typeof(T));
-                var stringWriter = new StringWriter();
-                using (var writer = XmlWriter.Create(stringWriter))
-                {
-                    xmlserializer.Serialize(writer, value);
-                    return stringWriter.ToString();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("An error occurred", ex);
-            }
-        }
-
-        public static T Deserialize<T>(this FileInfo xml) where T : class, new()
-        {
-            Debug.Assert(xml.Exists);
-
-            string xmlText = File.ReadAllText(xml.FullName);
-            return Deserialize<T>(xmlText);
-        }
-
-        public static T Deserialize<T>(this string xml) where T : class, new()
-        {
-            var serializer = new XmlSerializer(typeof(T));
-            using (var reader = new StringReader(xml))
-            {
-                try
-                {
-                    return (T)serializer.Deserialize(reader);
-                }
-                catch
-                { 
-                    return null;
-                }
-            }
-        }
     }
 }
