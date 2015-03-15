ddd16395c1801ab2fd89d727bafdfd28d699130d
Laoujin <woutervs@hotmail.com>
2015-03-15 18:39:55 +0100

    Version 1.0

--- TribalWars/Properties/AssemblyInfo.cs
+++ TribalWars/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sangu")]
 [assembly: AssemblyProduct("TW Tactics")]
-[assembly: AssemblyCopyright("Copyright © Sangu 2007 - 2014")]
+[assembly: AssemblyCopyright("Copyright © Sangu 2007 - 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
