d6c319a478c4a8be7857150b423d034f853c1e98
Laoujin <woutervs@hotmail.com>
2015-07-14 21:14:57 +0200

    Translations: BrowserControl

--- TribalWars/Browsers/Control/BrowserControl.designer.cs
+++ TribalWars/Browsers/Control/BrowserControl.designer.cs
@@ -28,208 +28,147 @@ namespace TribalWars.Browsers.Control
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BrowserControl));
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.BackButton = new System.Windows.Forms.ToolStripButton();
-            this.ForwardButton = new System.Windows.Forms.ToolStripButton();
-            this.StopButton = new System.Windows.Forms.ToolStripButton();
-            this.RefreshButton = new System.Windows.Forms.ToolStripButton();
-            this.HomeButton = new System.Windows.Forms.ToolStripButton();
-            this.HomeStatsButton = new System.Windows.Forms.ToolStripButton();
-            this.Browser = new System.Windows.Forms.WebBrowser();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.Url = new System.Windows.Forms.TextBox();
-            this.GoButton = new System.Windows.Forms.Button();
-            this.ParseResultLabel = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.Browser, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(313, 162);
-            this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BrowserControl));
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.BackButton = new System.Windows.Forms.ToolStripButton();
+			this.ForwardButton = new System.Windows.Forms.ToolStripButton();
+			this.StopButton = new System.Windows.Forms.ToolStripButton();
+			this.RefreshButton = new System.Windows.Forms.ToolStripButton();
+			this.HomeButton = new System.Windows.Forms.ToolStripButton();
+			this.HomeStatsButton = new System.Windows.Forms.ToolStripButton();
+			this.Browser = new System.Windows.Forms.WebBrowser();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.Url = new System.Windows.Forms.TextBox();
+			this.GoButton = new System.Windows.Forms.Button();
+			this.ParseResultLabel = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.toolStrip1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+			this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.Browser, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			// 
+			// toolStrip1
+			// 
+			resources.ApplyResources(this.toolStrip1, "toolStrip1");
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.BackButton,
             this.ForwardButton,
             this.StopButton,
             this.RefreshButton,
             this.HomeButton,
             this.HomeStatsButton});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStrip1.Size = new System.Drawing.Size(313, 25);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // BackButton
-            // 
-            this.BackButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.BackButton.Enabled = false;
-            this.BackButton.Image = ((System.Drawing.Image)(resources.GetObject("BackButton.Image")));
-            this.BackButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.BackButton.Name = "BackButton";
-            this.BackButton.Size = new System.Drawing.Size(23, 22);
-            this.BackButton.Text = "Back";
-            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
-            // 
-            // ForwardButton
-            // 
-            this.ForwardButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.ForwardButton.Enabled = false;
-            this.ForwardButton.Image = ((System.Drawing.Image)(resources.GetObject("ForwardButton.Image")));
-            this.ForwardButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.ForwardButton.Name = "ForwardButton";
-            this.ForwardButton.Size = new System.Drawing.Size(23, 22);
-            this.ForwardButton.Text = "Forward";
-            this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
-            // 
-            // StopButton
-            // 
-            this.StopButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.StopButton.Image = ((System.Drawing.Image)(resources.GetObject("StopButton.Image")));
-            this.StopButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.StopButton.Name = "StopButton";
-            this.StopButton.Size = new System.Drawing.Size(23, 22);
-            this.StopButton.Text = "Stop";
-            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
-            // 
-            // RefreshButton
-            // 
-            this.RefreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.RefreshButton.Image = ((System.Drawing.Image)(resources.GetObject("RefreshButton.Image")));
-            this.RefreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.RefreshButton.Name = "RefreshButton";
-            this.RefreshButton.Size = new System.Drawing.Size(23, 22);
-            this.RefreshButton.Text = "Refresh";
-            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
-            // 
-            // HomeButton
-            // 
-            this.HomeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
-            this.HomeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.HomeButton.Name = "HomeButton";
-            this.HomeButton.Size = new System.Drawing.Size(23, 22);
-            this.HomeButton.Text = "Tribal Wars home";
-            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
-            // 
-            // HomeStatsButton
-            // 
-            this.HomeStatsButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.HomeStatsButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeStatsButton.Image")));
-            this.HomeStatsButton.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.HomeStatsButton.Name = "HomeStatsButton";
-            this.HomeStatsButton.Size = new System.Drawing.Size(23, 22);
-            this.HomeStatsButton.Text = "TW Stats home";
-            this.HomeStatsButton.Click += new System.EventHandler(this.HomeStatsButton_Click);
-            // 
-            // Browser
-            // 
-            this.Browser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Browser.Location = new System.Drawing.Point(3, 53);
-            this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.Browser.Name = "Browser";
-            this.Browser.Size = new System.Drawing.Size(307, 106);
-            this.Browser.TabIndex = 1;
-            this.Browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.Browser_DocumentCompleted);
-            this.Browser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.Browser_Navigating);
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.Url);
-            this.panel1.Controls.Add(this.GoButton);
-            this.panel1.Controls.Add(this.ParseResultLabel);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 25);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(313, 25);
-            this.panel1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.Enabled = false;
-            this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 23);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Address";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // Url
-            // 
-            this.Url.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Url.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
-            this.Url.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
-            this.Url.Location = new System.Drawing.Point(57, 3);
-            this.Url.Name = "Url";
-            this.Url.Size = new System.Drawing.Size(232, 20);
-            this.Url.TabIndex = 4;
-            this.Url.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Url_KeyDown);
-            // 
-            // GoButton
-            // 
-            this.GoButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.GoButton.FlatAppearance.BorderSize = 0;
-            this.GoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.GoButton.Image = ((System.Drawing.Image)(resources.GetObject("GoButton.Image")));
-            this.GoButton.Location = new System.Drawing.Point(287, 2);
-            this.GoButton.Name = "GoButton";
-            this.GoButton.Size = new System.Drawing.Size(23, 19);
-            this.GoButton.TabIndex = 5;
-            this.GoButton.UseVisualStyleBackColor = true;
-            this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
-            // 
-            // ParseResultLabel
-            // 
-            this.ParseResultLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.ParseResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.ParseResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ParseResultLabel.Location = new System.Drawing.Point(3, 3);
-            this.ParseResultLabel.Name = "ParseResultLabel";
-            this.ParseResultLabel.Size = new System.Drawing.Size(48, 20);
-            this.ParseResultLabel.TabIndex = 6;
-            this.ParseResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.ParseResultLabel.Visible = false;
-            // 
-            // BrowserControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "BrowserControl";
-            this.Size = new System.Drawing.Size(313, 162);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.ResumeLayout(false);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+			// 
+			// BackButton
+			// 
+			resources.ApplyResources(this.BackButton, "BackButton");
+			this.BackButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.BackButton.Name = "BackButton";
+			this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+			// 
+			// ForwardButton
+			// 
+			resources.ApplyResources(this.ForwardButton, "ForwardButton");
+			this.ForwardButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.ForwardButton.Name = "ForwardButton";
+			this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+			// 
+			// StopButton
+			// 
+			resources.ApplyResources(this.StopButton, "StopButton");
+			this.StopButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.StopButton.Name = "StopButton";
+			this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+			// 
+			// RefreshButton
+			// 
+			resources.ApplyResources(this.RefreshButton, "RefreshButton");
+			this.RefreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.RefreshButton.Name = "RefreshButton";
+			this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+			// 
+			// HomeButton
+			// 
+			resources.ApplyResources(this.HomeButton, "HomeButton");
+			this.HomeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.HomeButton.Name = "HomeButton";
+			this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+			// 
+			// HomeStatsButton
+			// 
+			resources.ApplyResources(this.HomeStatsButton, "HomeStatsButton");
+			this.HomeStatsButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.HomeStatsButton.Name = "HomeStatsButton";
+			this.HomeStatsButton.Click += new System.EventHandler(this.HomeStatsButton_Click);
+			// 
+			// Browser
+			// 
+			resources.ApplyResources(this.Browser, "Browser");
+			this.Browser.Name = "Browser";
+			this.Browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.Browser_DocumentCompleted);
+			this.Browser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.Browser_Navigating);
+			// 
+			// panel1
+			// 
+			resources.ApplyResources(this.panel1, "panel1");
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.Url);
+			this.panel1.Controls.Add(this.GoButton);
+			this.panel1.Controls.Add(this.ParseResultLabel);
+			this.panel1.Name = "panel1";
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.ForeColor = System.Drawing.Color.Black;
+			this.label1.Name = "label1";
+			// 
+			// Url
+			// 
+			resources.ApplyResources(this.Url, "Url");
+			this.Url.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+			this.Url.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+			this.Url.Name = "Url";
+			this.Url.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Url_KeyDown);
+			// 
+			// GoButton
+			// 
+			resources.ApplyResources(this.GoButton, "GoButton");
+			this.GoButton.FlatAppearance.BorderSize = 0;
+			this.GoButton.Name = "GoButton";
+			this.GoButton.UseVisualStyleBackColor = true;
+			this.GoButton.Click += new System.EventHandler(this.GoButton_Click);
+			// 
+			// ParseResultLabel
+			// 
+			resources.ApplyResources(this.ParseResultLabel, "ParseResultLabel");
+			this.ParseResultLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.ParseResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.ParseResultLabel.Name = "ParseResultLabel";
+			// 
+			// BrowserControl
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "BrowserControl";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
 
         }
 
