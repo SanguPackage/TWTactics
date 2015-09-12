717896314def299de6f2fc48e802f1d8501ecf3f
Laoujin <woutervs@hotmail.com>
2015-09-12 18:54:24 +0200

    Translations: attackplan pane help layout change

--- TribalWars/Maps/AttackPlans/Controls/AttackHelpControl.Designer.cs
+++ TribalWars/Maps/AttackPlans/Controls/AttackHelpControl.Designer.cs
@@ -28,169 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AttackHelpControl));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.pictureBox2);
-            this.groupBox1.Controls.Add(this.pictureBox4);
-            this.groupBox1.Controls.Add(this.pictureBox3);
-            this.groupBox1.Controls.Add(this.pictureBox1);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label7);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(295, 484);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Help!";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::TribalWars.Properties.Resources.FlagBlue;
-            this.pictureBox2.Location = new System.Drawing.Point(152, 36);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(19, 18);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::TribalWars.Properties.Resources.cmdFind;
-            this.pictureBox4.Location = new System.Drawing.Point(6, 268);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(19, 18);
-            this.pictureBox4.TabIndex = 1;
-            this.pictureBox4.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = global::TribalWars.Properties.Resources.barracks;
-            this.pictureBox3.Location = new System.Drawing.Point(6, 150);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(19, 18);
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::TribalWars.Properties.Resources.PinSmall;
-            this.pictureBox1.Location = new System.Drawing.Point(108, 36);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(19, 18);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 248);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(279, 17);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Here comes the most interesting feature!";
-            // 
-            // label7
-            // 
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(56, 449);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(138, 20);
-            this.label7.TabIndex = 0;
-            this.label7.Text = "Happy hunting!";
-            // 
-            // label6
-            // 
-            this.label6.Location = new System.Drawing.Point(4, 402);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(285, 32);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Right click somewhere on the grass while in attack modus to change some general o" +
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AttackHelpControl));
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.pictureBox2);
+			this.groupBox1.Controls.Add(this.pictureBox4);
+			this.groupBox1.Controls.Add(this.pictureBox3);
+			this.groupBox1.Controls.Add(this.pictureBox1);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(3, 3);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(295, 484);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Help!";
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::TribalWars.Properties.Resources.FlagBlue;
+			this.pictureBox2.Location = new System.Drawing.Point(0, 43);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(19, 18);
+			this.pictureBox2.TabIndex = 2;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = global::TribalWars.Properties.Resources.cmdFind;
+			this.pictureBox4.Location = new System.Drawing.Point(6, 268);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(19, 18);
+			this.pictureBox4.TabIndex = 1;
+			this.pictureBox4.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = global::TribalWars.Properties.Resources.barracks;
+			this.pictureBox3.Location = new System.Drawing.Point(6, 150);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(19, 18);
+			this.pictureBox3.TabIndex = 1;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::TribalWars.Properties.Resources.PinSmall;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 19);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(19, 18);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(6, 248);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(279, 17);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Here comes the most interesting feature!";
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(56, 449);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(138, 20);
+			this.label7.TabIndex = 0;
+			this.label7.Text = "Happy hunting!";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(4, 402);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(285, 32);
+			this.label6.TabIndex = 0;
+			this.label6.Text = "Right click somewhere on the grass while in attack modus to change some general o" +
     "ptions.";
-            // 
-            // label5
-            // 
-            this.label5.Location = new System.Drawing.Point(4, 334);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(285, 56);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Pretty much everything in this attack panel reacts to mouseclicks: clicking villa" +
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(4, 334);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(285, 56);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "Pretty much everything in this attack panel reacts to mouseclicks: clicking villa" +
     "ge names will select them, double click will center the map over them or use rig" +
     "ht click for more options.";
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(31, 265);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(258, 56);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "It searches in your villages to find those that can still reach the target at the" +
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(31, 265);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(258, 56);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "It searches in your villages to find those that can still reach the target at the" +
     " given speed and adds them as attackers (or as defenders if you like!).\r\nIf you " +
     "need to be quick about it, this is it!";
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(31, 150);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(258, 98);
-            this.label2.TabIndex = 0;
-            this.label2.Text = resources.GetString("label2.Text");
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(7, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(282, 127);
-            this.label1.TabIndex = 0;
-            this.label1.Text = resources.GetString("label1.Text");
-            // 
-            // label8
-            // 
-            this.label8.Location = new System.Drawing.Point(128, 38);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(27, 13);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "and";
-            // 
-            // AttackHelpControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.Controls.Add(this.groupBox1);
-            this.Name = "AttackHelpControl";
-            this.Size = new System.Drawing.Size(284, 189);
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(31, 150);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(258, 98);
+			this.label2.TabIndex = 0;
+			this.label2.Text = resources.GetString("label2.Text");
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(18, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(257, 127);
+			this.label1.TabIndex = 0;
+			this.label1.Text = resources.GetString("label1.Text");
+			// 
+			// AttackHelpControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScroll = true;
+			this.Controls.Add(this.groupBox1);
+			this.Name = "AttackHelpControl";
+			this.Size = new System.Drawing.Size(267, 172);
+			this.groupBox1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
@@ -207,7 +197,6 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label5;
     }
 }
