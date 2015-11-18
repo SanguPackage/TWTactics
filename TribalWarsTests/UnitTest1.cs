1527d6be4d46923bb9cdc965343d022865d19402
Laoujin <woutervs@hotmail.com>
2015-11-18 19:34:53 +0100

    start TEST

--- TribalWarsTests/UnitTest1.cs
+++ TribalWarsTests/UnitTest1.cs
@@ -18,27 +18,30 @@ namespace TribalWarsTests
         [TestMethod]
         public void Display()
         {
-            //var path = @"C:\unix\code\TribalWars\TWTactics\TribalWars\WorldTemplate\WorldSettings.xml";
-            //var obj = WorldConfiguration.LoadFromFile(path);
+	        var x = Convert.ToChar(System.ConsoleKey.Oem2);
 
-            //var wvv = new WorldViewsView();
-            //obj.Views.Add(wvv);
+	        //var path = @"C:\unix\code\TribalWars\TWTactics\TribalWars\WorldTemplate\WorldSettings.xml";
+	        //var obj = WorldConfiguration.LoadFromFile(path);
 
-            //wvv.Category = "cat1";
+	        //var wvv = new WorldViewsView();
+	        //obj.Views.Add(wvv);
 
-            //var dr = new WorldViewsViewDrawersDrawer();
-            //dr.Icon = "icon";
-            //dr.Type = "type";
-            //wvv.Drawers.Add(dr);
+	        //wvv.Category = "cat1";
+
+	        //var dr = new WorldViewsViewDrawersDrawer();
+	        //dr.Icon = "icon";
+	        //dr.Type = "type";
+	        //wvv.Drawers.Add(dr);
 
 
-            
 
 
-            //Assert.AreEqual("{WorldName}", obj.Name);
-            //Assert.AreEqual(3, obj.Data.Count(), "Data.Village / Player / Tribe not loaded");
-            
-            //obj.SaveToFile(@"c:\temp\test.txt");
+
+	        //Assert.AreEqual("{WorldName}", obj.Name);
+	        //Assert.AreEqual(3, obj.Data.Count(), "Data.Village / Player / Tribe not loaded");
+
+	        //obj.SaveToFile(@"c:\temp\test.txt");
+			Assert.AreEqual(x, ConsoleKey.Q);
         }
     }
 }
