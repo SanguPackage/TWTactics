b01bc7e9db197b0daba5e1315d5f2746230c1bc5
Laoujin <woutervs@hotmail.com>
2015-11-18 20:32:16 +0100

    TEST: logging

--- TribalWars/Program.cs
+++ TribalWars/Program.cs
@@ -1,36 +1,45 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Threading;
 using System.Windows.Forms;
 using TribalWars.Forms;
 
 namespace TribalWars
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
 			try
 			{
-				var ci = CultureInfo.GetCultureInfo(Properties.Settings.Default.Culture);
-				Thread.CurrentThread.CurrentCulture = ci;
-				Thread.CurrentThread.CurrentUICulture = ci;
+				try
+				{
+					var ci = CultureInfo.GetCultureInfo(Properties.Settings.Default.Culture);
+					Thread.CurrentThread.CurrentCulture = ci;
+					Thread.CurrentThread.CurrentUICulture = ci;
+				}
+				catch
+				{
+					Properties.Settings.Default.Culture = "";
+					Properties.Settings.Default.Save();
+				}
+
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+
+				Application.Run(new MainForm());
 			}
-			catch
+			catch (Exception ex)
 			{
-				Properties.Settings.Default.Culture = "";
-				Properties.Settings.Default.Save();
+				File.WriteAllText(@"c:\Users\vagrant\Desktop\TWTactics\twtactics.txt", ex.ToString());
+				MessageBox.Show(ex.ToString(), "oepsie");
 			}
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            Application.Run(new MainForm());
-        }
-    }
+		}
+	}
 }
